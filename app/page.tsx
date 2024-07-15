beea6402929f6fae2c44f9554547d7560c5c88f0
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-07-15 15:10:58 +0900

    refactor: Remove unused chrome authority code

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,6 @@ import Image from "next/image";
 import { useEffect } from "react";
 
 export default function Home() {
-    var AudioContext;
-    var audioContext;
-    useEffect(() => {
-        window.onload = function() {
-            navigator.mediaDevices.getUserMedia({ audio: true }).then(() => {
-                AudioContext = window.AudioContext || window.AudioContext;
-                audioContext = new AudioContext();
-            }).catch(e => {
-                console.error(`Audio permissions denied: ${e}`);
-            });
-    }
-    }, [])
 
     return (
         <main>
